4bb48ae3ec3788f06d85b7bdf300b0a59c104594
akashdesaimasai <akash.desai3105@gail.com>
2024-01-01 10:55:36 +0530

    refactor: added a empty object as a prop to onOpen

--- components/Navigation/CreateServerButton.tsx
+++ components/Navigation/CreateServerButton.tsx
@@ -11,7 +11,7 @@ const CreateServerButton = () => {
   return (
     <ToolTip content={"Create a Server"}>
       <div className="h-16 w-16 rounded-full bg-accent text-emerald-400 hover:rounded-3xl hover:bg-emerald-500 hover:text-white flex justify-center items-center font-semibold  transition-hover cursor-pointer">
-        <button onClick={()=>onOpen("createServer")}>
+        <button onClick={()=>onOpen("createServer",{})}>
           <Plus className="h-8 w-8" />
         </button>
       </div>
